a65d1b7ba6d4ec144de74efb1c693d0f5009fef3
Daniel Berger <dberger@globe.gov>
2010-02-04 09:43:46 -0700

    Added tests for endpoint, namespace, and trusted attributes.

--- test/test_mail_sympa.rb
+++ test/test_mail_sympa.rb
@@ -4,21 +4,78 @@
 # This is the test suite for the mail-sympa library. You should run
 # these tests via the test rake task.
 ########################################################################
+require 'rubygems'
+gem 'test-unit'
+
 require 'test/unit'
+require 'resolv'
 require 'mail/sympa'
 
 class MailSympaTest < Test::Unit::TestCase
-  def setup
-    @mail = Mail::Sympa.new
+  def self.startup
+    @@url = "http://" + Resolv.getaddress('sympa') + "/sympasoap"
   end
 
-  def test_server_basic_functionality
-    assert_respond_to(@mail, :server)
-    assert_nothing_raised{ @mail.server }
-    assert_kind_of(String, @mail.server)
+  def setup
+    @mail = Mail::Sympa.new(@@url)
+    @user = 'postmaster@' # Add your domain here
+    @pass = 'XXXXXX'      # Set the passwd here
+  end
+
+  # Because most methods won't work without logging in first
+  def login
+    @mail.login(@user, @passwd)
+  end
+
+  test "endpoint method basic functionality" do
+    assert_respond_to(@mail, :endpoint)
+    assert_nothing_raised{ @mail.endpoint }
+    assert_kind_of(String, @mail.endpoint)
+  end
+
+  test "endpoint method returns expected result" do
+    assert_equal(@@url, @mail.endpoint)
+  end
+
+  test "endpoint method is a readonly attribute" do
+    assert_raise(NoMethodError){ @mail.endpoint = 'foo' }
+  end
+
+  test "trusted method basic functionality" do
+    assert_respond_to(@mail, :trusted)
+    assert_nothing_raised{ @mail.trusted }
+    assert_boolean(@mail.trusted)
+  end
+
+  test "trusted method expected result" do
+    assert_false(@mail.trusted)
+  end
+
+  test "trusted method is a readonly attribute" do
+    assert_raise(NoMethodError){ @mail.trusted = false }
+  end
+
+  test "namespace method basic functionality" do
+    assert_respond_to(@mail, :namespace)
+    assert_nothing_raised{ @mail.namespace }
+    assert_kind_of(String, @mail.namespace)
+  end
+
+  test "namespace method returns expected result" do
+    assert_equal("urn:sympasoap", @mail.namespace)
+  end
+
+  test "namespace is a readonly attrubite" do
+    assert_raise(NoMethodError){ @mail.namespace = 'foo' }
   end
 
   def teardown
     @mail = nil
+    @user = nil
+    @pass = nil
+  end
+
+  def self.shutdown
+    @@url = nil
   end
 end
