a66738215e5aea4915cc83a21d54b1a9698593e9
Daniel Berger <dberger@globe.gov>
2010-02-04 11:15:20 -0700

    Added tests for the lists method.

--- test/test_mail_sympa.rb
+++ test/test_mail_sympa.rb
@@ -17,14 +17,15 @@ class MailSympaTest < Test::Unit::TestCase
   end
 
   def setup
-    @mail = Mail::Sympa.new(@@url)
-    @user = 'postmaster@' # Add your domain here
-    @pass = 'XXXXXX'      # Set the passwd here
+    @mail  = Mail::Sympa.new(@@url)
+    @user  = 'postmaster' # Add your domain here
+    @pass  = 'XXXXXXXX'   # Set the password here
+    @topic = 'testlist'
   end
 
   # Because most methods won't work without logging in first
   def login
-    @mail.login(@user, @passwd)
+    @mail.login(@user, @pass)
   end
 
   test "endpoint method basic functionality" do
@@ -65,14 +66,56 @@ class MailSympaTest < Test::Unit::TestCase
     assert_equal("urn:sympasoap", @mail.namespace)
   end
 
-  test "namespace is a readonly attrubite" do
+  test "namespace method is a readonly attrubite" do
     assert_raise(NoMethodError){ @mail.namespace = 'foo' }
+  end
+
+  test "login method basic functionality" do
+    assert_respond_to(@mail, :login)
+  end
+
+  test "login method works as expected with proper credentials" do
+    assert_nothing_raised{ @mail.login(@user, @pass) }
+    assert_not_nil(@mail.cookie)
+    assert_kind_of(String, @mail.login(@user, @pass))
+  end
+
+  test "login method raises an error if the credentials are invalid" do
+    assert_raise(SOAP::FaultError){ @mail.login('bogus', 'bogus') }
+  end
+
+  test "login method requires two arguments" do
+    assert_raise(ArgumentError){ @mail.login }
+    assert_raise(ArgumentError){ @mail.login('bogus') }
+  end
+
+  test "lists method basic functionality" do
+    login
+    assert_respond_to(@mail, :lists)
+    assert_nothing_raised{ @mail.lists }
+  end
+
+  test "lists method with no arguments returns all lists" do
+    login
+    assert_kind_of(Array, @mail.lists)
+    assert_kind_of(String, @mail.lists.first)
+  end
+
+  test "lists method accepts a topic and subtopic" do
+    login
+    assert_kind_of(Array, @mail.lists(@topic))
+    assert_kind_of(Array, @mail.lists(@topic, @topic))
+  end
+
+  test "lists method accepts a maximum of two arguments" do
+    assert_raise(ArgumentError){ @mail.lists(@topic, @topic, @topic) }
   end
 
   def teardown
     @mail = nil
     @user = nil
     @pass = nil
+    @topic = nil
   end
 
   def self.shutdown
